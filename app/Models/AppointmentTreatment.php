935cf3602bc3f06263849d87e4ddda97be798215
menna <me>
2025-11-17 14:25:39 +0200

    Migration

---
+++ app/Models/AppointmentTreatment.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AppointmentTreatment extends Model
+{
+    //
+}
---
+++ database/migrations/2025_11_17_121446_create_appointment_treatments_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointment_treatments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('appointment_id')->constrained()->onDelete('cascade');
+            $table->foreignId('treatment_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointment_treatments');
+    }
+};
